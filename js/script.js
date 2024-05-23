971cc4620a35d0c909835a41c482e8b8f41f04fc
AdelMysterio <storliniadelmo@gmail.com>
2024-05-23 20:11:03 +0200

    corretto snack-1

--- js/script.js
+++ js/script.js
@@ -1,56 +1,15 @@
 /* Snack 1 */
 
-const invitati = [
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Brad Pitt',
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Johnny Depp',
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Lady Gaga',
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Cristiano Ronaldo',
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Georgina Rodriguez',
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Chiara Ferragni',
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'George Clooney',
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Amal Clooney',
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Fedez',
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Amadeus',
-    },
-    {
-        tableName: 'Tavolo Vip',
-        guestName: 'Fiorello',
-    },
-]
+const nominativi = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
 
-invitati.forEach((element, i) => {
-    element.place = i + 1
-    console.log(element)
+const invitati = nominativi.map((element, i) => {
+    return {guestName : nominativi[i],
+        guestTable: 'Tavolo Vip',
+        place: i+1
+    }
 });
+
+console.log(invitati)
 
 
 /* Snack 2 */
