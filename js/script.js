3f9afd7f12d7d67a2b0141539742f933af22cb0c
Adelmysterio <storliniadelmo@gmail.com>
2024-05-23 14:47:47 +0200

    svolto snack-2

--- js/script.js
+++ js/script.js
@@ -48,9 +48,66 @@ const invitati = [
 ]
 
 invitati.forEach((element, i) => {
-    element.place = i+1
+    element.place = i + 1
     console.log(element)
 });
 
 
 /* Snack 2 */
+
+const studenti = [
+    {
+        id: 213,
+        name: 'Giuseppina della Rovere',
+        grades: 78
+    },
+    {
+        id: 110,
+        name: 'Paola Cortellessa',
+        grades: 96
+    },
+    {
+        id: 250,
+        name: 'Andrea Mantegna',
+        grades: 48
+    },
+    {
+        id: 145,
+        name: 'Gaia Borromini',
+        grades: 74
+    },
+    {
+        id: 196,
+        name: 'Luigi Grimaldello',
+        grades: 68
+    },
+    {
+        id: 102,
+        name: 'Piero dell Francesca',
+        grades: 50
+    },
+    {
+        id: 120,
+        name: 'Francesca da Polenta',
+        grades: 84
+    },
+];
+
+const nomiStudenti = studenti.map((element) => element.name);
+
+const votoStudenti = [];
+const idVotoStudenti = [];
+
+studenti.forEach(element => {
+    if (element.grades > 70 && element.id > 120) {
+        idVotoStudenti.push(element)
+    };
+
+    if (element.grades > 70) {
+        votoStudenti.push(element)
+    };
+});
+
+console.log(nomiStudenti);
+console.log(votoStudenti);
+console.log(idVotoStudenti);
