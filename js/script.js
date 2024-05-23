4b8cf86199d27f7bd27ae25604f6303314b16e40
AdelMysterio <storliniadelmo@gmail.com>
2024-05-23 13:40:54 +0200

    svolto snack-1

--- js/script.js
+++ js/script.js
@@ -1,65 +1,56 @@
-// Crea un array di oggetti che rappresentano delle persone, 10.
-// Ogni persona ha un nome, un cognome e un'età.
+/* Snack 1 */
 
-// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
+const invitati = [
+    {
+        tableName: 'Tavolo Vip',
+        guestName: 'Brad Pitt',
+    },
+    {
+        tableName: 'Tavolo Vip',
+        guestName: 'Johnny Depp',
+    },
+    {
+        tableName: 'Tavolo Vip',
+        guestName: 'Lady Gaga',
+    },
+    {
+        tableName: 'Tavolo Vip',
+        guestName: 'Cristiano Ronaldo',
+    },
+    {
+        tableName: 'Tavolo Vip',
+        guestName: 'Georgina Rodriguez',
+    },
+    {
+        tableName: 'Tavolo Vip',
+        guestName: 'Chiara Ferragni',
+    },
+    {
+        tableName: 'Tavolo Vip',
+        guestName: 'George Clooney',
+    },
+    {
+        tableName: 'Tavolo Vip',
+        guestName: 'Amal Clooney',
+    },
+    {
+        tableName: 'Tavolo Vip',
+        guestName: 'Fedez',
+    },
+    {
+        tableName: 'Tavolo Vip',
+        guestName: 'Amadeus',
+    },
+    {
+        tableName: 'Tavolo Vip',
+        guestName: 'Fiorello',
+    },
+]
 
-const persone = [
-    {
-        nome: 'Gina',
-        cognome: 'Ginetti',
-        eta: '16'
-    },
-    {
-        nome: 'Pina',
-        cognome: 'Linetti',
-        eta: '14'
-    },
-    {
-        nome: 'Rina',
-        cognome: 'Finetti',
-        eta: '19'
-    },
-    {
-        nome: 'Lina',
-        cognome: 'Fonetti',
-        eta: '24'
-    },
-    {
-        nome: 'Xena',
-        cognome: 'Principessa Guerriera',
-        eta: '50'
-    },
-    {
-        nome: 'Annamaria',
-        cognome: 'Franzoni',
-        eta: '56'
-    },
-    {
-        nome: 'Bill',
-        cognome: 'Clinton',
-        eta: '70'
-    },
-    {
-        nome: 'Leornardo',
-        cognome: 'Di Caprio',
-        eta: '25'
-    },
-    {
-        nome: 'Gesu',
-        cognome: 'Cristo',
-        eta: '33'
-    },
-    {
-        nome: 'Dante',
-        cognome: 'Alighieri',
-        eta: '759'
-    },
-];
-
-const personeModificate = persone.forEach(element => {
-    if (parseInt(element.eta) >= 18) {
-        console.log(element.nome, element.cognome, 'Può Guidare')
-    }
-    else {
-    console.log(element.nome, element.cognome, 'Non può Guidare')}
+invitati.forEach((element, i) => {
+    element.place = i+1
+    console.log(element)
 });
+
+
+/* Snack 2 */
